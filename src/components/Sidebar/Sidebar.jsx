c42c146986c91b16df341d018344a1233eae046d
adityapandey14 <anshu61796@gmail.com>
2024-05-30 13:49:40 +0530

    First push

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import { Flex, Box , Link } from '@chakra-ui/react'
+import React from 'react'
+import { Link as RouterLink} from 'react-router-dom'
+import { InstagramLogo } from '../../assets/contants'
+
+const Sidebar = () => {
+  return (
+   <Box height={"100vh"} borderRight={"1px solid"} borderColor = {"whiteAlpha.300"} py={8} position ={"sticky"} top ={0} left={0} px={{base:2, md:4}} >
+    <Flex direction={"column"} gap={10} w="full" height={"full"} >
+        <Link to={"/"} as={RouterLink} pl={2} display={{base: "none" , md:"block"}} cursor={"pointer"} >
+          <InstagramLogo/>
+        </Link>
+
+    </Flex>
+
+   </Box>
+  )
+}
+
+export default Sidebar
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { mode } from '@chakra-ui/theme-tools';
+
+// Customize the theme
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAlpha.900")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config, styles });
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
